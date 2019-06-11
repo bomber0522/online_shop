5a74af6fcee318c4a3af82a2098effcb68fe38df
Takaki <takaki55730317@gmail.com>
2019-06-12 00:25:33 +0900

    remember_digest

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,5 @@
 class Member < ApplicationRecord
+  attr_accessor :remember_token
   before_save :downcase_email
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -7,4 +8,17 @@ class Member < ApplicationRecord
                uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  def Member.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = Member.new_token
+    update_attribute(:remember_digest, Member.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
