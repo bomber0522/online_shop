a45b7483d0d11363eb10db901266111efcc8acae
Takaki <takaki55730317@gmail.com>
2019-06-17 17:19:05 +0900

    relationships_ajax

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module OnlineShop
 
     config.time_zone = "Tokyo"
     config.i18n.default_locale = :ja
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
